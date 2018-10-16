54b8481da0b28bb71831df1097f70fdbbf0801dd
Aditya Kulkarni <adityapk@gmail.com>
2018-10-16 14:08:08 -0700

    Add release script

---
+++ src/scripts/mkrelease.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z $QT_STATIC ]; then echo "QT_STATIC is not set"; exit 1; fi
+if [ -z $APP_VERSION ]; then echo "APP_VERSION is not set"; exit 1; fi
+if [ -z $PREV_VERSION ]; then echo "PREV_VERSION is not set"; exit 1; fi
+
+echo "Updating version numbers"
+# Replace the version number in the .pro file so it gets picked up everywhere
+sed -i "s/${PREV_VERSION}/${APP_VERSION}/g" zcash-qt-wallet.pro > /dev/null
+
+# Also update it in the README.md
+sed -i "s/${PREV_VERSION}/${APP_VERSION}/g" README.md > /dev/null
+
+echo "Configuring"
+make distclean > /dev/null
+$QT_STATIC/bin/qmake zcash-qt-wallet.pro -spec linux-clang CONFIG+=release > /dev/null
+
+echo "Building"
+rm -rf bin/zcash-qt-wallet* > /dev/null
+make -j$(nproc) > /dev/null
+
+echo "Packaging"
+mkdir bin/zcash-qt-wallet-v$APP_VERSION > /dev/null
+cp zcash-qt-wallet bin/zcash-qt-wallet-v$APP_VERSION > /dev/null
+cp README.md bin/zcash-qt-wallet-v$APP_VERSION > /dev/null
+cp LICENSE bin/zcash-qt-wallet-v$APP_VERSION > /dev/null
+tar cvf bin/zcash-qt-wallet-v$APP_VERSION.tar.gz bin/zcash-qt-wallet-v$APP_VERSION/ > /dev/null
+
+echo "Done"
