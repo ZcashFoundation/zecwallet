fb310097f3c856d7c6b150c7a49b465947db21fb
Aditya Kulkarni <adityapk@gmail.com>
2018-10-31 15:52:32 -0700

    version number to installer

--- src/scripts/mkwininstaller.ps1
+++ src/scripts/mkwininstaller.ps1
@@ -11,5 +11,5 @@ Copy-Item src/scripts/zec-qt-wallet.wxs release/wininstaller/
 candle.exe release/wininstaller/zec-qt-wallet.wxs -o release/wininstaller/zec-qt-wallet.wixobj 
 light.exe -ext WixUIExtension -cultures:en-us release/wininstaller/zec-qt-wallet.wixobj -out release/wininstaller/zec-qt-wallet.msi 
 
-New-Item artifacts -itemtype directory | Out-Null
+New-Item artifacts -itemtype directory -Force | Out-Null
 Copy-Item release/wininstaller/zec-qt-wallet.msi ./artifacts/$target.msi
