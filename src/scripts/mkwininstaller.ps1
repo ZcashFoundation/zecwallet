03ca804ab38c7160b639925af393710aa4e88d3f
Aditya Kulkarni <adityapk@gmail.com>
2018-11-13 11:07:49 -0800

    Windows installer

--- src/scripts/mkwininstaller.ps1
+++ src/scripts/mkwininstaller.ps1
@@ -1,4 +1,8 @@
-if (-not (Test-Path env:APP_VERSION)) { echo "APP_VERSION is not set. Please set it to the version you want to build, like 0.1.6"; exit; }
+if (-not (Test-Path env:APP_VERSION)) { 
+    echo "APP_VERSION is not set. Please set it to the version you want to build"; 
+    echo '$Env:APP_VERSION = "0.3.0"'
+    exit; 
+}
 $target="zec-qt-wallet-v$Env:APP_VERSION"
 
 echo "Building Installer"
@@ -6,7 +10,12 @@ Remove-Item -Path release/wininstaller -Recurse | Out-Null
 New-Item release/wininstaller -itemtype directory | Out-Null
 
 Copy-Item release/$target/zec-qt-wallet.exe release/wininstaller/
-Copy-Item src/scripts/zec-qt-wallet.wxs release/wininstaller/
+Copy-Item release/$target/LICENSE release/wininstaller/
+Copy-Item release/$target/README.md release/wininstaller/
+Copy-Item release/$target/zcashd.exe release/wininstaller/
+Copy-Item release/$target/zcash-cli.exe release/wininstaller/
+
+cat src/scripts/zec-qt-wallet.wxs | % { $_ -replace "RELEASE_VERSION", "$Env:APP_VERSION" } > release/wininstaller/zec-qt-wallet.wxs
 
 candle.exe release/wininstaller/zec-qt-wallet.wxs -o release/wininstaller/zec-qt-wallet.wixobj 
 light.exe -ext WixUIExtension -cultures:en-us release/wininstaller/zec-qt-wallet.wixobj -out release/wininstaller/zec-qt-wallet.msi 
