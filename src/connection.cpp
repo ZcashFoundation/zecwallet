efe74e7ebe4f6c92eb9b0b3331ffc2266e07c2b4
earthorbit <55867375+earthorbit@users.noreply.github.com>
2019-09-27 09:00:16 -0600

    Update connection.cpp

--- src/connection.cpp
+++ src/connection.cpp
@@ -697,8 +697,6 @@ void Connection::doRPC(const json& payload, const std::function<void(json)>& cb,
         return;
     }
 
-    qDebug() << "RPC: " << QString::fromStdString(payload["method"]);
-    qDebug() << "< payload " << QString::fromStdString(payload.dump());
 
     QNetworkReply *reply = restclient->post(*request, QByteArray::fromStdString(payload.dump()));
 
