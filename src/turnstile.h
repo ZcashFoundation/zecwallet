6ebca12255df6a784fba47a3b5a41c8f141575ad
Aditya Kulkarni <adityapk@gmail.com>
2019-10-07 12:38:44 -0700

    Remove old sapling turnstile

--- src/turnstile.h
+++ src/turnstile.h
@@ -3,75 +3,18 @@
 
 #include "precompiled.h"
 
-class RPC;
 class MainWindow;
-struct Tx;
-
-
-struct TurnstileMigrationItem {
-    QString     fromAddr;
-    QString        intTAddr;
-    QString        destAddr;
-    int            blockNumber;
-    double        amount;
-    int         status;
-};
-
-enum TurnstileMigrationItemStatus {
-    NotStarted = 0,
-    SentToT,
-    SentToZS,
-    NotEnoughBalance,
-    UnknownError
-};
-
-struct ProgressReport {
-    int     step;
-    int     totalSteps;
-    int     nextBlock;
-    bool     hasErrors;
-    QString from;
-    QString to;
-    QString via;
-};
 
 class Turnstile
 {
 public:
-    Turnstile(RPC* _rpc, MainWindow* mainwindow);
-
-    void               planMigration(QString zaddr, QString destAddr, int splits, int numBlocks);
-    QList<double>      splitAmount(double amount, int parts);
-    void               fillAmounts(QList<double>& amounts, double amount, int count);
-
-    QList<TurnstileMigrationItem> readMigrationPlan();
-    void               writeMigrationPlan(QList<TurnstileMigrationItem> plan);
-    void               removeFile();
-    
-    void               executeMigrationStep();
-    ProgressReport     getPlanProgress();
-    bool               isMigrationPresent();
-
     static void        showZcashdMigration(MainWindow* parent);
 
-    static double       minMigrationAmount;
-private:
-    QList<int>          getBlockNumbers(int start, int end, int count);
-    QString             writeableFile();
-
-    void                doSendTx(Tx tx, std::function<void(void)> cb);
-
-
-    QList<TurnstileMigrationItem>::Iterator getNextStep(QList<TurnstileMigrationItem>& plan);    
-
-    RPC*         rpc;    
-    MainWindow* mainwindow;
+    static double       minMigrationAmount;   
 };
 
 
 // Classes for zcashd 2.0.5 native migration
-
-
 class MigrationTxns : public QAbstractTableModel {
 
 public:
