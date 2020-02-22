bcb1899568ba701fcfc28dd69b11ba1939abd2c2
Aditya Kulkarni <adityapk@gmail.com>
2020-02-21 19:32:59 -0800

    Disable auto update

--- app/main.dev.js
+++ app/main.dev.js
@@ -19,7 +19,7 @@ export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
     autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
+    // autoUpdater.checkForUpdatesAndNotify();
   }
 }
 
