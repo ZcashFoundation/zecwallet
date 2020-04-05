c24d1f98a452212c2ec4fe22c51169da44f530bd
Aditya Kulkarni <zcash@adityapk.com>
2020-04-05 14:25:11 -0700

    Load only 100 txns at a time

--- app/components/Transactions.js
+++ app/components/Transactions.js
@@ -250,14 +250,15 @@ type Props = {
 
 type State = {
   clickedTx: Transaction | null,
-  modalIsOpen: boolean
+  modalIsOpen: boolean,
+  numTxnsToShow: number
 };
 
 export default class Transactions extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { clickedTx: null, modalIsOpen: false };
+    this.state = { clickedTx: null, modalIsOpen: false, numTxnsToShow: 100 };
   }
 
   txClicked = (tx: Transaction) => {
@@ -270,9 +271,17 @@ export default class Transactions extends Component<Props, State> {
     this.setState({ clickedTx: null, modalIsOpen: false });
   };
 
+  show100MoreTxns = () => {
+    const { numTxnsToShow } = this.state;
+
+    this.setState({ numTxnsToShow: numTxnsToShow + 100 });
+  };
+
   render() {
     const { transactions, info, addressBook, setSendTo } = this.props;
-    const { clickedTx, modalIsOpen } = this.state;
+    const { clickedTx, modalIsOpen, numTxnsToShow } = this.state;
+
+    const isLoadMoreEnabled = numTxnsToShow < transactions.length;
 
     const addressBookMap = addressBook.reduce((map, obj) => {
       // eslint-disable-next-line no-param-reassign
@@ -293,7 +302,7 @@ export default class Transactions extends Component<Props, State> {
             <div className={[cstyles.center, cstyles.margintoplarge].join(' ')}>No Transactions Yet</div>
           )}
           {transactions &&
-            transactions.map(t => {
+            transactions.slice(0, numTxnsToShow).map(t => {
               const key = t.type + t.txid + t.address;
               return (
                 <TxItemBlock
@@ -306,6 +315,16 @@ export default class Transactions extends Component<Props, State> {
                 />
               );
             })}
+
+          {isLoadMoreEnabled && (
+            <div
+              style={{ marginLeft: '45%', width: '100px' }}
+              className={cstyles.primarybutton}
+              onClick={this.show100MoreTxns}
+            >
+              Load more
+            </div>
+          )}
         </ScrollPane>
 
         <TxModal
