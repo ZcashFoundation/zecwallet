fff5bba6e7d6437fd0e11b65cf4d65fa98e4b1a3
Aditya Kulkarni <adityapk@gmail.com>
2020-11-18 13:08:51 -0800

    0.9.21

--- app/companion.js
+++ app/companion.js
@@ -389,7 +389,7 @@ export default class CompanionAppListener {
       maxzspendable,
       tokenName,
       zecprice,
-      serverversion: '0.9.20'
+      serverversion: '0.9.21'
     };
 
     return JSON.stringify(resp);
--- bin/printversion.ps1
+++ bin/printversion.ps1
@@ -1,1 +1,1 @@
-echo "::set-env name=VERSION::0.9.20"
+echo "::set-env name=VERSION::0.9.21"
